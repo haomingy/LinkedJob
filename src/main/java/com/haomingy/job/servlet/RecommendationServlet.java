7e170cea4790c796d3d77c3e53deea5a57242457
haomingy <45500578+haomingy@users.noreply.github.com>
2021-07-08 21:43:51 -0400

    add servlet and entity class

--- src/main/java/com/haomingy/job/servlet/RecommendationServlet.java
+++ src/main/java/com/haomingy/job/servlet/RecommendationServlet.java
@@ -7,13 +7,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "TestServlet")
-public class TestServlet extends HttpServlet {
+@WebServlet(name = "RecommendationServlet", urlPatterns = {"/recommendation"})
+public class RecommendationServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("This is RecommendationServlet");
 
     }
 }
---
+++ src/main/java/com/haomingy/job/servlet/SearchServlet.java
@@ -0,0 +1,28 @@
+package com.haomingy.job.servlet;
+
+import com.haomingy.job.external.GitHubClient;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "SearchServlet", urlPatterns = {"/search"})
+public class SearchServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        double lat = Double.parseDouble(request.getParameter("lat"));
+        double lon = Double.parseDouble(request.getParameter("lon"));
+
+        GitHubClient client = new GitHubClient();
+        String itemsString = client.search(lat, lon, null);
+        response.setContentType("application/json");
+        response.getWriter().print(itemsString);
+    }
+
+}
